71dd315e97415b570114a09cee9b1cd43cb7fd78
Anwar Isied <anwar.isied@gmail.com>
2026-04-09 11:36:40 +0300

    dynamic array - template

--- header/dynamic_array.h
+++ header/dynamic_array.h
@@ -7,24 +7,100 @@
 #include <iosfwd>
 #include <iostream>
 
+template<typename T>
 class dynamic_array {
 private:
-    int *arr=nullptr;
+    T *arr=nullptr;
     int last_index;  // index of last element (-1 means empty)
     int capacity;  // number of elements
-    [[nodiscard]] bool is_full() const;
-    [[nodiscard]] bool is_empty() const;
-    void resize();
-    [[nodiscard]] int* copy(const dynamic_array &other) const;
+    [[nodiscard]] bool is_full() const {
+        return this->capacity==this->last_index+1;
+    }
+    [[nodiscard]] bool is_empty() const {
+        return this->last_index==-1;
+    }
+
+    void resize() {
+        if (is_full()) {
+            const int new_capacity=this->capacity*2;
+            const auto new_arr=new T[new_capacity];
+            for (int i=0;i<=this->last_index;i++) {
+                new_arr[i]=this->arr[i];
+            }
+            delete[] this->arr;
+            this->arr=new_arr;
+            this->capacity=new_capacity;
+        }
+    }
+    [[nodiscard]] T* copy(const dynamic_array &other) const {
+        const auto new_arr = new T[other.capacity];
+        if (other.arr!=nullptr) {
+            for (int i = 0; i <= other.last_index; i++) {
+                new_arr[i] = other.arr[i];
+                std::cout<<new_arr[i];
+            }
+        }
+        return new_arr;
+    }
 public:
-    explicit dynamic_array(int capacity=4);
-    dynamic_array(const dynamic_array& other);
-    dynamic_array& operator=(const dynamic_array& other);
-    void push_back(int value);
-    void push_front(int value);
-    int pop_back();
-    int pop_front();
-    ~dynamic_array();
+    explicit dynamic_array(int capacity=4) {
+        capacity=capacity==0?1:capacity;
+        this->capacity=capacity;
+        this->last_index=-1;
+        this->arr=new T[capacity];
+    }
+
+    explicit dynamic_array(const dynamic_array &other) : last_index(other.last_index), capacity(other.capacity) {
+        this->arr = copy(other);
+    }
+    dynamic_array& operator=(const dynamic_array &other) {
+        if (this==&other) {
+            return *this;
+        }
+        delete[] arr;
+        this->capacity = other.capacity;
+        this->last_index = other.last_index;
+
+        this->arr = copy(other);
+        return *this;
+    }
+    void push_front(const T value) {
+        resize();
+        //Shift the elements
+        for (int i=last_index;i>=0;i--) {
+            this->arr[i+1]=this->arr[i];
+        }
+        this->arr[0]=value;
+        this->last_index=this->last_index+1;
+    }
+
+    void push_back(const T value) {
+        resize();
+        arr[++last_index] = value;
+    }
+
+    T pop_back() {
+        if (is_empty()) {
+            throw std::runtime_error("Array is empty");
+        }
+        const T value=this->arr[this->last_index];
+        this->last_index=this->last_index-1;
+        return value;
+    }
+    T pop_front() {
+        if (is_empty()) {
+            throw std::runtime_error("Array is empty");
+        }
+        const T value=this->arr[0];
+        for (int i=0;i<this->last_index;i++) {
+            this->arr[i]=this->arr[i+1];
+        }
+        this->last_index=this->last_index-1;
+        return value;
+    }
+    ~dynamic_array() {
+        delete[] this->arr;
+    }
     friend std::ostream& operator<<(std::ostream& os, const dynamic_array& obj) {
         os << "Last index: " << obj.last_index<<std::endl;
         os << "Capacity:" <<obj.capacity<<std::endl;
--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include "./header/dynamic_array.h"
 #include <iostream>
 int main() {
-    auto d_arr=dynamic_array(4);
+    auto d_arr=dynamic_array<int>(4);
     d_arr.push_back(10);
     d_arr.push_back(12);
     d_arr.push_front(15);
-    d_arr.pop_front();
-    d_arr.pop_front();
     std::cout << d_arr;
     return 0;
 }
